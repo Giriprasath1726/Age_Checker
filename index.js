48baed1fc9010c74847990f6a906572888e0e78c
Giri prasath <140329858+Giriprasath1726@users.noreply.github.com>
2025-04-11 22:40:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function checkAge() {
       result.textContent = "You are a minor.";
     } else if (age >= 18 && age < 60) {
       result.textContent = "You are an adult.";
-    } else if (age >= 60) {
-      result.textContent = "You are a senior.";
     } else if (age > 100) {
       result.textContent = "You should be dead right now !";
+    } else if (age >= 60 && age <=100) {
+      result.textContent = "You are a senior.";
     } else {
       result.textContent = "Please enter a valid age.";
     }
