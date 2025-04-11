df5209fedb6e7764d225dc74919afcb5b5dde65c
Giri prasath <140329858+Giriprasath1726@users.noreply.github.com>
2025-04-11 22:34:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function checkAge() {
+    const age = document.getElementById("ageInput").value;
+    const result = document.getElementById("result");
+  
+    if (age === "") {
+        result.textContent = "Please enter your age.";
+    } else if (age < 18) {
+      result.textContent = "You are a minor.";
+    } else if (age >= 18 && age < 60) {
+      result.textContent = "You are an adult.";
+    } else if (age >= 60) {
+      result.textContent = "You are a senior.";
+    } else {
+      result.textContent = "Please enter a valid age.";
+    }
+}
+  
